e3f8e8ebc4d1b0bb58aca387bf922e916e79f247
Jose-Dell22 <jfdell41@gmail.com>
2025-07-04 13:11:50 -0500

    Configuración url deploy forntend

--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://cursor-ai-xi.vercel.app/';
 
 export const apiConfig = {
   baseUrl: API_BASE_URL,
